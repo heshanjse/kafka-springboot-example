872a3384750e0be0b18a40ad4666d29dd9037dd4
HeshanJay <psaheshan@gmail.com>
2024-02-16 23:35:59 +0800

    add example project

---
+++ src/main/java/com/hesh/springboot/SpringbootKafkaExampleApplication.java
@@ -0,0 +1,15 @@
+package com.hesh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+
+@SpringBootApplication
+public class SpringbootKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaExampleApplication.class, args);
+	}
+
+
+}
